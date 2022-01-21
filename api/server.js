52fffd5090716d9b47adfc9b7c4ecee3ad652e5b
ambrojessica <ambrojessica0@gmail.com>
2022-01-21 14:12:17 -0600

    small changes, migration complete. starting model and routing

--- api/server.js
+++ api/server.js
@@ -1,21 +1,22 @@
 // build your server here and require it from index.js
 const express = require('express');
-const server = express();
 
-// const projectsRouter = require('./project/router');
+const projectRouter = require('./project/router');
 // const resourceRouter = require('./resource/router');
 // const taskRouter = require('./task/router');
 
-server.use(express.json());
-// server.use('/api/project', projectsRouter);
-// server.use('/api/resource', resourceRouter);
-// server.use('/api/task', taskRouter);
+const server = express();
 
-// server.use((err, req, res, next) => {
-//   res.status(err.status || 500).json({
-//     message: err.message,
-//     stack: err.stack,
-//   });
-// });
+server.use(express.json());
+server.use('/api/projects', projectRouter);
+// server.use('/api/resources', resourceRouter);
+// server.use('/api/tasks', taskRouter);
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 module.exports = server;
